8fc2605562defa1083e0284c7cbd1c8396b96247
scw000000 <scw000000@gmail.com>
2017-08-29 14:52:52 -0400

    2nd version of FPS camera control, make it more smooth

--- Game/Assets/Scripts/AvatarControl.cs
+++ Game/Assets/Scripts/AvatarControl.cs
@@ -3,58 +3,55 @@ using System.Collections;
 
 public class AvatarControl : MonoBehaviour
    {
-   public float m_MovingSpeed = 15.0f;
-   public float m_ScalingSpeed = 3.0f;
-   public const float m_RotatingSpeed = 500.0f;
-   private Rigidbody m_RigidBody = null;
-   private Vector3 m_PrevMousePos;
-   private Vector3 m_CurrMousePos;
+   public float MovingSpeed = 15.0f;
+   public float ScalingSpeed = 3.0f;
+   public const float RotatingSpeed = 500.0f;
+   private Rigidbody RigidBody = null;
+   private GameObject CameraRoot;
 
-   // Use this for initialization
-   void Start()
+    // Use this for initialization
+    void Start()
       {
       Debug.Log( gameObject.name );
-      m_RigidBody = gameObject.GetComponent<Rigidbody>();
-      if( m_RigidBody == null )
+      RigidBody = gameObject.GetComponent<Rigidbody>();
+      if( RigidBody == null )
          {
          Debug.LogError( "Cannot find rigid body of the avatar" );
          }
 
-      m_PrevMousePos = Input.mousePosition;
-      }
+        CameraRoot = gameObject.transform.Find("CameraRoot").gameObject;
+    }
 
    // Update is called once per frame
    void Update()
       {
       if( Input.GetKey( KeyCode.A ) )
          {
-         gameObject.transform.Translate( new Vector3( -1.0f * Time.deltaTime * m_MovingSpeed, 0.0f, 0.0f ) );
+         gameObject.transform.Translate( new Vector3( -1.0f * Time.deltaTime * MovingSpeed, 0.0f, 0.0f ) );
          }
 
       if( Input.GetKey( KeyCode.D ) )
          {
-         gameObject.transform.Translate( new Vector3( Time.deltaTime * m_MovingSpeed, 0.0f, 0.0f ) );
+         gameObject.transform.Translate( new Vector3( Time.deltaTime * MovingSpeed, 0.0f, 0.0f ) );
          }
 
       if( Input.GetKey( KeyCode.W ) )
          {
-         gameObject.transform.Translate( new Vector3( 0.0f, 0.0f, Time.deltaTime * m_MovingSpeed ) );
+         gameObject.transform.Translate( new Vector3( 0.0f, 0.0f, Time.deltaTime * MovingSpeed ) );
          }
 
       if( Input.GetKey( KeyCode.S ) )
          {
-         gameObject.transform.Translate( new Vector3( 0.0f, 0.0f, -1.0f * Time.deltaTime * m_MovingSpeed ) );
+         gameObject.transform.Translate( new Vector3( 0.0f, 0.0f, -1.0f * Time.deltaTime * MovingSpeed ) );
          }
-
-      m_CurrMousePos = Input.mousePosition;
+      
       if( Input.GetKey( KeyCode.Mouse1 ) )
          {
-            // gameObject.transform.RotateAround( gameObject.transform.position, gameObject.transform.right, -1.0f * deltaMousePos.y * Time.deltaTime * m_RotationSpeed );
-            //Debug.Log(Input.GetAxis("Mouse X"));
-            
-            gameObject.transform.RotateAround( gameObject.transform.position, new Vector3( 0.0f, 1.0f, 0.0f ), Input.GetAxis("Mouse X") * Time.deltaTime * m_RotatingSpeed );
+            gameObject.transform.RotateAround( gameObject.transform.position, new Vector3( 0.0f, 1.0f, 0.0f ), Input.GetAxis("Mouse X") * Time.deltaTime * RotatingSpeed );
+
+
+            CameraRoot.transform.RotateAround(CameraRoot.transform.position, gameObject.transform.right, -1.0f * Input.GetAxis("Mouse Y") * Time.deltaTime * RotatingSpeed );
          }
-      m_PrevMousePos = m_CurrMousePos;
 
       }
    }
--- Game/Assets/Scripts/WorldSwitch.cs
+++ Game/Assets/Scripts/WorldSwitch.cs
@@ -3,32 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldSwitch : MonoBehaviour {
-    public Camera m_CameraA;
-    public Camera m_CameraB;
-    public RenderTexture m_RenderTexture;
-    public GameObject m_Avatar;
+    public GameObject CameraSetPrefab;
+
+    private GameObject CameraRoot;
+    private GameObject CameraSet;
+    private GameObject HoldingObject;
+    private Camera CameraA;
+    private Camera CameraB;
+    public RenderTexture RenderTexture;
 	// Use this for initialization
 	void Start () {
-	}
+        if (CameraSetPrefab == null) {
+            Debug.LogError("CameraSetPrefab is not defined");
+            return;
+        }
+        CameraRoot = gameObject.transform.Find("CameraRoot").gameObject;
+        if (CameraRoot == null) {
+            Debug.LogError("Cannot find camera root");
+        }
+        
+        CameraSet = Instantiate(CameraSetPrefab, CameraRoot.transform.position, CameraRoot.transform.rotation);
+        HoldingObject = CameraSet.transform.Find("Holder").gameObject;
+        HoldingObject.layer = LayerMask.NameToLayer("WorldA");
+        // cameraSetInstance.transform.parent = CameraRoot.transform;
+        CameraA = CameraSet.transform.Find("CameraA").gameObject.GetComponent<Camera>();
+        CameraB = CameraSet.transform.Find("CameraB").gameObject.GetComponent<Camera>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        // update postion here to get smooth camera control
+        CameraSet.transform.position = CameraRoot.transform.position;
+        CameraSet.transform.rotation = Quaternion.Slerp(CameraSet.transform.rotation, CameraRoot.transform.rotation, 10f * Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.X)) {
-            var collider = m_Avatar.GetComponent<CapsuleCollider>();
-            var overlappers = Physics.OverlapCapsule(m_Avatar.transform.position, m_Avatar.transform.position, collider.radius);
+            var collider = gameObject.GetComponent<CapsuleCollider>();
+            var overlappers = Physics.OverlapCapsule(gameObject.transform.position, gameObject.transform.position, collider.radius);
             bool switchable = true;
+            // Only when the player is not
             foreach (var overlap in overlappers) {
-                if (overlap.gameObject.layer != m_Avatar.layer) {
+                if (overlap.gameObject.layer != gameObject.layer) {
                     switchable = false;
                 }
             }
             if (switchable) {
                 Debug.Log("Switch!");
-                gameObject.layer = LayerMask.NameToLayer((m_CameraA.targetTexture == null ? "WorldB" : "WorldA"));
-                m_Avatar.layer = LayerMask.NameToLayer((m_CameraA.targetTexture == null ? "WorldB" : "WorldA"));
-                var sceneCamera = m_CameraA.targetTexture == null ? m_CameraA : m_CameraB;
-                var backgroundCamera = sceneCamera.GetInstanceID() == m_CameraA.GetInstanceID() ? m_CameraB : m_CameraA;
-                sceneCamera.targetTexture = m_RenderTexture;
+                gameObject.layer = LayerMask.NameToLayer((CameraA.targetTexture == null ? "WorldB" : "WorldA"));
+                HoldingObject.layer = gameObject.layer;
+                var sceneCamera = CameraA.targetTexture == null ? CameraA : CameraB;
+                var backgroundCamera = sceneCamera.GetInstanceID() == CameraA.GetInstanceID() ? CameraB : CameraA;
+                sceneCamera.targetTexture = RenderTexture;
                 backgroundCamera.targetTexture = null;
             }
             else{
