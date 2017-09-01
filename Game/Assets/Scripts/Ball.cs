350b5b432254caa18318ac93f353071ee7491155
Shicheng Chu <cheng61746093@gmail.com>
2017-09-01 19:05:05 -0400

    Ball Respawn and physics
    I finished Ball respawn, launch and part of physics (collision handler)

---
+++ Game/Assets/Scripts/Ball.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private GameObject player;
+	private bool isAttached;
+	private Vector3 velocity;
+	private Vector3 locShift;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		isAttached = true;
+		velocity = new Vector3 (0.0f,0.0f,0.0f);
+		locShift = new Vector3 (0.0f,0.0f,1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdatePosition ();
+
+	}
+
+	void UpdatePosition(){
+
+		if (isAttached) {
+			if (player) {
+				transform.position = player.transform.position + locShift;
+
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					Launch ();
+				}
+			}
+		} else {
+			transform.Translate (velocity*Time.deltaTime);
+			if(Input.GetKeyDown (KeyCode.R)){
+				Respawn();
+			}
+		}
+	}
+
+	void Launch(){
+		if(isAttached){
+			if (Input.GetKey(KeyCode.D)) {
+				velocity = new Vector3 (9.0f, 0.0f, 12.0f);
+			} else if (Input.GetKey(KeyCode.A)) {
+				velocity = new Vector3 (-9.0f, 0.0f, 12.0f);
+			} else {
+				velocity = new Vector3 (0.0f, 0.0f, 12.0f);
+			}
+		}
+		isAttached = false;
+	}
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "Wall"){
+			ChangeXVelocity ();
+		}
+		if(col.gameObject.tag == "PlayerTag"){
+			ChangeZVelocity ();
+		}
+	}
+	void Respawn(){
+		player = GameObject.Find ("Player");
+		isAttached = true;
+		velocity = new Vector3 (0.0f,0.0f,0.0f);
+		locShift = new Vector3 (0.0f,0.0f,1.0f);
+		transform.position = player.transform.position + locShift;
+	}
+
+	public void ChangeXVelocity(){
+		velocity = new Vector3 (-velocity.x,velocity.y,velocity.z);
+	}
+
+	public void ChangeZVelocity(){
+		velocity = new Vector3 (velocity.x,velocity.y,-velocity.z);
+	}
+}
