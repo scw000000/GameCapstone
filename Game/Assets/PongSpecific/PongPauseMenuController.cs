97078dfac226fc040a39a8c13b61f6f81a9fa758
Yifei Zhao <readzhao@yifeis-mbp.lan>
2017-09-01 22:23:27 -0400

    Add pause menu to the pong game

---
+++ Game/Assets/PongSpecific/PongPauseMenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongPauseMenuController : MonoBehaviour {
+	//For future use to disable player
+	//public Transform Player;
+	public Transform canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			this.Pause();
+		}
+	}
+
+	public void Pause(){
+		if (canvas.gameObject.activeInHierarchy == false) {
+			canvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+			//When the player controller is ready, this line of code will pause user control
+			//Player.GetComponent<PonePlayerController> ().enable = false;
+		} else {
+			canvas.gameObject.SetActive (false);
+			Time.timeScale = 1;
+			//When the player controller is ready, this line of code will resume user control
+			//Player.GetComponent<PonePlayerController> ().enable = true;
+		}
+	}
+}
