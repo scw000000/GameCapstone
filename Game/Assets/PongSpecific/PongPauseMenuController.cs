995fd9b52909309e02696b501605d13cdaf4cdc6
Yifei Zhao <readzhao@gmail.com>
2017-09-04 12:57:09 -0400

    Reenable pause function in the game.

--- Game/Assets/PongSpecific/PongPauseMenuController.cs
+++ Game/Assets/PongSpecific/PongPauseMenuController.cs
@@ -22,12 +22,12 @@ public class PongPauseMenuController : MonoBehaviour {
     public void Pause(){
 		if (canvas.gameObject.activeInHierarchy == false) {
 			canvas.gameObject.SetActive (true);
-			//Time.timeScale = 0;
+			Time.timeScale = 0;
 			//When the player controller is ready, this line of code will pause user control
 			//Player.GetComponent<PonePlayerController> ().enable = false;
 		} else {
 			canvas.gameObject.SetActive (false);
-			//Time.timeScale = 1;
+			Time.timeScale = 1;
 			//When the player controller is ready, this line of code will resume user control
 			//Player.GetComponent<PonePlayerController> ().enable = true;
 		}
