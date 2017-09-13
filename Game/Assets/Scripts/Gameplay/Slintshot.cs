4f652cf8e1aca832e2be7074e74f3266db86bde5
DESKTOP-A37QAAU\iGeNius <cheng61746093@gmail.com>
2017-09-13 11:49:54 -0400

    slingshot
    Add a slingshot for puzzles. Still has bug that cannot pull the string

---
+++ Game/Assets/Scripts/Gameplay/Slintshot.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slintshot : MonoBehaviour {
+
+    private Vector3 grabPos;
+
+    private LineRenderer left;
+    private LineRenderer right;
+    private GameObject ball;
+    private GameObject player;
+    private bool canGrab;
+	// Use this for initialization
+	void Start () {
+        left = transform.Find("LeftAttachPoint").transform.GetComponent<LineRenderer>();
+        right = transform.Find("RightAttachPoint").transform.GetComponent<LineRenderer>();
+        ball = 
+        player = GameObject.FindGameObjectWithTag("Player");
+        canGrab = false;
+    }
+
+    private void OnTriggerEnter(Collision obj)
+    {
+        Physics.IgnoreCollision(obj.transform.GetComponent<Collider>(), transform.GetComponent<Collider>(), true);
+    }
+
+    void OnTriggerStay(Collision obj)
+    {
+        if (obj.gameObject.tag.Equals("Player"))
+        {
+            canGrab = true;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (canGrab)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                grabPos = Camera.main.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+                ball.transform.position = grabPos;
+                left.SetPosition(0, new Vector3(grabPos.x, grabPos.y, grabPos.z));
+                right.SetPosition(0, new Vector3(grabPos.x, grabPos.y, grabPos.z));
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+
+
+                ball.transform.position = grabPos;
+
+                left.SetPosition(0, new Vector3(grabPos.x, grabPos.y, grabPos.z ));
+                right.SetPosition(0, new Vector3(grabPos.x, grabPos.y, grabPos.z));
+
+                Vector3 Vec3L = new Vector3(-2.0f - grabPos.x, 2.0f - grabPos.y, -grabPos.z);
+                Vector3 Vec3R = new Vector3(2.0f - grabPos.x, 2.0f - grabPos.y, -grabPos.z);
+                Vector3 Dir = (Vec3L + Vec3R).normalized;
+
+                ball.transform.GetComponent<Rigidbody>().useGravity = true;
+                ball.transform.GetComponent<Rigidbody>().AddForce(Dir * 9.0f, ForceMode.Impulse);
+
+                left.SetPosition(0, new Vector3(0F, 1.8F, 0F));
+                right.SetPosition(0, new Vector3(0F, 1.8F, 0F));
+            }
+
+            canGrab = false;
+        }
+	}
+}
