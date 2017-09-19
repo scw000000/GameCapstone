daff2534a0262831175d11f67268baa80f7fb6e8
DESKTOP-A37QAAU\iGeNius <cheng61746093@gmail.com>
2017-09-19 01:48:32 -0400

    Finished MazeRotation and cube trigger
    Finished MazeRotation and cube trigger for mazer rotation. Build a playable scene for slingshot and maze rotation. Small Bugs exits

---
+++ Game/Assets/Scripts/Gameplay/CubeTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTrigger : MonoBehaviour {
+
+    public MazeRotation _maze; 
+	// Use this for initialization
+	void Start () {
+        _maze = GameObject.FindGameObjectWithTag("Maze").GetComponent<MazeRotation>();
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag.Equals("TriggerBall"))
+        {
+            _maze.Rotate90();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Game/Assets/Scripts/Gameplay/MazeRotation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeRotation : MonoBehaviour {
+
+    private float rotateSpeed;
+    private bool isDuringRotation;
+    
+    private float deltaDegree;
+	// Use this for initialization
+	void Start () {
+        isDuringRotation = false;
+        rotateSpeed = 30.0f;
+        deltaDegree = 0.0f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Rotate90();
+            //isDuringRotation = true;
+            //Quaternion destRotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if (isDuringRotation) {
+            //transform.rotation = Quaternion.Lerp(transform.rotation, destRotation, Time.time * rotateSpeed);
+            transform.RotateAround(transform.position, transform.up,  Time.deltaTime*rotateSpeed);
+            deltaDegree += Time.deltaTime * rotateSpeed;
+        }
+
+
+
+
+        if (deltaDegree > 90.0f) {
+            isDuringRotation = false;
+            deltaDegree = 0.0f;
+            Debug.Log("Rotating false");
+        }
+
+    }
+
+    public void Rotate90()
+    {
+        if (!isDuringRotation)
+        {
+            isDuringRotation = true;
+            //destRotation.RotateAround(transform.position, transform.up, 90.0f);
+            //Debug.Log("Rotate90 true");
+            //Debug.Log(destRotation.ToString());
+        }
+        /*else
+        {
+            Debug.Log("Rotate90 true");
+        }*/
+    }
+}
