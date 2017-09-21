334675baecf3cd4baabab6e42235296c41f68b90
scw000000 <scw000000@gmail.com>
2017-09-20 22:00:59 -0400

    Make laser -portal interaction more accurate

---
+++ Game/Assets/Scripts/Game Framework/LaserPortalSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPortalSwitcher : MonoBehaviour {
+    public GameObject _laserObject;
+    private int _worldALayer;
+    private int _worldBLayer;
+    private int _worldAInPortalLayer;
+    private int _worldBInPortalLayer;
+    void Start()
+    {
+        _worldALayer = LayerMask.NameToLayer("WorldA");
+        _worldBLayer = LayerMask.NameToLayer("WorldB");
+        _worldAInPortalLayer = LayerMask.NameToLayer("WorldAInPortal");
+        _worldBInPortalLayer = LayerMask.NameToLayer("WorldBInPortal");
+    }
+
+    void Update()
+    {
+        if (gameObject.layer == _worldALayer || gameObject.layer == _worldBLayer)
+        {
+            _laserObject.layer = gameObject.layer;
+        }
+        else if (gameObject.layer == _worldAInPortalLayer || gameObject.layer == _worldBInPortalLayer)
+        {
+            _laserObject.layer = gameObject.layer == _worldAInPortalLayer ? _worldBLayer : _worldALayer;
+        }
+    }
+}
--- Game/Assets/Scripts/LaserSwitch.cs
+++ Game/Assets/Scripts/LaserSwitch.cs
@@ -7,18 +7,7 @@ public class LaserSwitch : MonoBehaviour
     GameObject laserObject;
     private bool triggered;
     Vector3 initPos;
-    private int _worldALayer;
-    private int _worldBLayer;
-    private int _worldAInPortalLayer;
-    private int _worldBInPortalLayer;
-    void Start()
-    {
-        _worldALayer = LayerMask.NameToLayer("WorldA");
-        _worldBLayer = LayerMask.NameToLayer("WorldB");
-        _worldAInPortalLayer = LayerMask.NameToLayer("WorldAInPortal");
-        _worldBInPortalLayer = LayerMask.NameToLayer("WorldBInPortal");
-    }
-
+    
     void SwitchOn()
     {
         //Debug.Log("Switched on");
@@ -26,14 +15,4 @@ public class LaserSwitch : MonoBehaviour
          laserObject.SendMessage("LaserOn");
     }
 
-    void Update()
-    {
-        laserObject = GameObject.FindGameObjectWithTag("Laser");
-        if (gameObject.layer == _worldALayer || gameObject.layer == _worldBLayer) {
-            laserObject.layer = gameObject.layer;
-        }
-        else if (gameObject.layer == _worldAInPortalLayer || gameObject.layer == _worldBInPortalLayer) {
-            laserObject.layer = gameObject.layer == _worldAInPortalLayer ? _worldBLayer: _worldALayer;
-        }
-    }
 }
