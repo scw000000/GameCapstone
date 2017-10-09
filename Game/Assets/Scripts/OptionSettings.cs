3d082db1d7c5b082c7c9d8d099b67424500afee3
panchenhui <pan.558@osu.edu>
2017-10-09 01:41:07 -0400

    added video settings

---
+++ Game/Assets/Scripts/OptionSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionSettings : MonoBehaviour {
+
+	//public Toggle[] resolutionToggles;
+	public Dropdown dropdown;
+
+	public int[] screenWidths;
+	public int[] screenHeights;
+	int activeScreenResIndex;
+
+	public void SetScreenResolution(int i){
+		if (i == dropdown.value) {
+			activeScreenResIndex = i;
+			Screen.SetResolution(screenWidths[i], screenHeights[i], false);
+		}
+	}
+
+	public void SetFullscreen (bool isFullscreen){
+			dropdown.interactable = !isFullscreen;
+
+		if(isFullscreen){
+			Resolution[] allResolutions = Screen.resolutions;
+			Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+			Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+		}else{
+			SetScreenResolution(activeScreenResIndex);
+		}
+	}
+}
---
+++ Game/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+	public GameObject options;
+
+	public void SetScreenResolution(int i){
+	
+	}
+
+	public void SetFullscreen(bool isFullscreen){
+	}
+
+}
