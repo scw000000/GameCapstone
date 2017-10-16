dbcddf9689aff55d3218240acf78b804aae1a599
es5379843 <31444482+es5379843@users.noreply.github.com>
2017-10-16 05:26:14 -0400

    LaserPuzzle in IceWorld

---
+++ Game/Assets/SpawnObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+    public GameObject _spawnObjects;
+    private int _laserSpawnCheck;
+    // Use this for initialization
+    void Start () {
+        _laserSpawnCheck = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TriggerEvent()
+    {
+        _laserSpawnCheck += 1;
+        if (_laserSpawnCheck == 1)
+        {
+            _spawnObjects.SetActive(true);
+        }
+
+    }
+}
