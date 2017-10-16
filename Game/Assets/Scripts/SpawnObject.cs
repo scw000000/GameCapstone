ddf9e95282ce815f941af57219e0cbef1217e8e8
es5379843 <31444482+es5379843@users.noreply.github.com>
2017-10-16 02:56:18 -0400

    Revert "Revert "LaserPuzzle/Ice World update""
    This reverts commit 84e85fb8beafc6fd2fad6c4fe42c3185cf58f045.

---
+++ Game/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+    public GameObject _laserPuzzle;
+    private int _laserSpawnCheck;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void TriggerEvent()
+    {
+        _laserSpawnCheck += 1;
+        if (_laserSpawnCheck == 1)
+        {
+            _laserPuzzle.SetActive(true);
+        }
+
+    }
+}
