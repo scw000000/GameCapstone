c7b50caf8b159d4a2076c9b55875df4239655a80
es5379843 <31444482+es5379843@users.noreply.github.com>
2017-10-16 02:30:30 -0400

    LaserPuzzle/Ice World update

---
+++ Game/Assets/Scripts/ElevatingObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatingObject : MonoBehaviour {
+
+    public float _goalHeight;
+    private bool _ifElevating;
+    private float _direction;
+    private float _moveSpeed;
+    public GameObject _myObject;
+
+	// Use this for initialization
+	void Start () {
+        //_goalHeight = -0.5f;
+        _ifElevating = false;
+        _direction = 1.0f;
+        _moveSpeed = 3.0f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name.Equals("Ball"))
+        {
+            _ifElevating = true;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (_ifElevating && _myObject.transform.position.y < -_goalHeight)
+        {
+            _myObject.transform.Translate(Vector3.up * Time.deltaTime * _moveSpeed * _direction, Space.World);
+        }
+	}
+}
