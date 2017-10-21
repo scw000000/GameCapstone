307b6c6d31c1303f5c573d48d6b301c68db00791
scw000000 <scw000000@gmail.com>
2017-10-21 18:12:00 -0400

    Fixed lens flare and start making jumppad cave

---
+++ Game/Assets/Scripts/Cave Specific/LensFlareReset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LensFlareReset : MonoBehaviour {
+    public GameObject _lensFlareObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            _lensFlareObject.GetComponent<LensFlareControl>()._avaliable = true;
+            _lensFlareObject.GetComponent<LensFlare>().enabled = true;
+            // Disable itself as well to prevent it turn on lensflare again
+            //  enabled = false;
+        }
+    }
+}
--- Game/Assets/Scripts/Graphics/LensFlareControl.cs
+++ Game/Assets/Scripts/Graphics/LensFlareControl.cs
@@ -8,57 +8,58 @@ public class LensFlareControl : MonoBehaviour {
     private GameObject _player;
     private WorldSwitch _playerSwitchComp;
     private LensFlare _lensFlare;
+    public bool _avaliable = true;
     private WorldSwitchSphere _cameraASwitchComp;
     private WorldSwitchSphere _cameraBSwitchComp;
     // Use this for initialization
- //   void Start () {
- //       Invoke("Init", 4);    
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _existLayer = LayerMask.NameToLayer(_existInLayerName);
+        _playerSwitchComp = _player.GetComponent<WorldSwitch>();
+        _lensFlare = gameObject.GetComponent<LensFlare>();
+    }
 
-	//}
+    //// Update is called once per frame
+    void Update()
+    {
+        if (_avaliable && ( ( _player.layer == _existLayer && !_player.GetComponent<WorldSwitch>()._insidePortal ) ||
+            (_player.layer != _existLayer && _player.GetComponent<WorldSwitch>()._insidePortal) )
+        //_player.layer == (_existInLayerName == "WorldA" ? LayerMask.NameToLayer("WorldAInPortal") : LayerMask.NameToLayer("WorldBInPortal"))
+                )
+        {
+            if (_cameraASwitchComp == null) {
+                var camSet = GameObject.FindGameObjectWithTag("MainCamera");
+                _cameraASwitchComp = GameObject.Find("CameraA").gameObject.GetComponent<WorldSwitchSphere>();
+                _cameraBSwitchComp = GameObject.Find("CameraB").gameObject.GetComponent<WorldSwitchSphere>();
+            }
+            // only when the light is inside the switch sphere do we need to open lens flare effect
+            
+            float distance = Vector3.Distance(gameObject.transform.position, _player.transform.position);
+            if ((!_cameraASwitchComp.enabled && !_cameraBSwitchComp.enabled)
+                || distance < (_cameraASwitchComp.enabled ? _cameraASwitchComp._currSphereRadius : _cameraBSwitchComp._currSphereRadius))
+            {
+                _lensFlare.enabled = true;
+            }
+            else
+            {
+                _lensFlare.enabled = false;
+            }
+            // _lensFlare.enabled = true;
+        }
+        else {
+            _lensFlare.enabled = false;
+        }
+    }
 
- //   void Init() {
- //       do
- //       {
- //           _player = GameObject.FindGameObjectWithTag("Player");
- //       }
- //       while (_player == null);
- //       _existLayer = LayerMask.NameToLayer(_existInLayerName);
- //       _playerSwitchComp = _player.GetComponent<WorldSwitch>();
- //       _lensFlare = gameObject.GetComponent<LensFlare>();
- //       var camSet = GameObject.FindGameObjectWithTag("MainCamera");
- //       _cameraASwitchComp = camSet.transform.Find("CameraA").gameObject.GetComponent<WorldSwitchSphere>();
- //       _cameraBSwitchComp = camSet.transform.Find("CameraB").gameObject.GetComponent<WorldSwitchSphere>();
- //   }
-	
-	//// Update is called once per frame
-	//void Update () {
- //       if (_player != null) {
- //           if (_player.layer == _existLayer && !_playerSwitchComp._insidePortal)
- //           {
- //               // only when the light is inside the switch sphere do we need to open lens flare effect
- //               float distance = Vector3.Distance(gameObject.transform.position, _player.transform.position);
- //               if ((!_cameraASwitchComp.enabled && !_cameraBSwitchComp.enabled)
- //                   || distance < (_cameraASwitchComp.enabled ? _cameraASwitchComp._currSphereRadius : _cameraBSwitchComp._currSphereRadius))
- //               {
- //                   _lensFlare.enabled = true;
- //               }
- //               else {
- //                   _lensFlare.enabled = false;
- //               }
- //           }
- //           else
- //           {
- //               _lensFlare.enabled = false;
- //           }
- //       }
- //   }
-
- //   void OnTriggerEnter(Collider other) {
- //       if (other.gameObject.tag.Equals("Player")) {
- //           Debug.Log("OUUUUUT");
- //         //  _lensFlare.enabled = false;
- //           // Disable itself as well to prevent it turn on lensflare again
- //         //  enabled = false;
- //       }
- //   }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            _avaliable = false;
+            _lensFlare.enabled = false;
+            // Disable itself as well to prevent it turn on lensflare again
+            //  enabled = false;
+        }
+    }
 }
