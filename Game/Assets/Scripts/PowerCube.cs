b05bbd02809167b817a49184dd24076f32a4ed81
panchenhui <pan.558@osu.edu>
2017-11-06 00:46:40 -0500

    Added destorable wall

---
+++ Game/Assets/Scripts/PowerCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerCube: MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "Destoryable") {
+			Destroy (col.gameObject);
+		}
+	}
+}
