fa8e91d82005f4692ba4208035b7601cf6c9f754
panchenhui <pan.558@osu.edu>
2017-11-12 03:18:26 -0500

    working on the new level

---
+++ Game/Assets/Scripts/ButtonOpenDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonOpenDoor : MonoBehaviour {
+
+	public GameObject Door1;
+	public GameObject Door2;
+
+	// Use this for initialization
+	void Start () {
+		Door2.SetActive(true);
+		Door1.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		Door2.SetActive(false);
+		Door1.SetActive(true);
+	}
+
+	void OnTriggerExit(Collider other){
+		Door2.SetActive(true);
+		Door1.SetActive(false);
+	}
+}
---
+++ Game/Assets/Scripts/PlatformHoldPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformHoldPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			Debug.Log ("YES!!!");
+			other.transform.parent = this.transform;
+		}
+	}
+
+//	void OnTriggerExit(Collider other){
+//		if (other.tag == "Player") {
+//			other.transform.parent = null;
+//		}
+//	}
+}
