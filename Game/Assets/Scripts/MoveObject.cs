87c962b85e4bbca77506ca429014183087a7ef6a
panchenhui <pan.558@osu.edu>
2017-11-13 21:02:35 -0500

    updated pick up

--- Game/Assets/Scripts/MoveObject.cs
+++ Game/Assets/Scripts/MoveObject.cs
@@ -31,36 +31,53 @@ public class MoveObject : MonoBehaviour {
 //		}
 	}
 
-	void OnCollisionEnter(Collision col){
-//		if (col.gameObject.tag == "Player") {
-//			if (Input.GetKeyDown (KeyCode.E)) {
-//				item.GetComponent<Rigidbody> ().useGravity = false;
-//				item.GetComponent<Rigidbody> ().isKinematic = true;
-//				item.transform.position = guide.transform.position;
-//				item.transform.rotation = guide.transform.rotation;
-//				item.transform.parent = tempParent.transform;
-//			}
-//
-//			if (Input.GetKeyUp (KeyCode.E)) {
-//				item.GetComponent<Rigidbody> ().useGravity = true;
-//				item.GetComponent<Rigidbody> ().isKinematic = false;
-//				item.transform.parent = null;
-//				item.transform.position = guide.transform.position;
-//			}
-//		}
-	}
-	void OnMouseDown(){
-		item.GetComponent<Rigidbody> ().useGravity = false;
-		item.GetComponent<Rigidbody> ().isKinematic = true;
-		item.transform.position = guide.transform.position;
-		item.transform.rotation = guide.transform.rotation;
-		item.transform.parent = tempParent.transform;
+	void OnTriggerStay(Collider col){
+		if (col.gameObject.tag == "Player"){
+			Debug.Log ("coll");
+			if (Input.GetKeyDown (KeyCode.E)) {
+				Debug.Log ("key");
+				item.GetComponent<Rigidbody> ().useGravity = false;
+				item.GetComponent<Rigidbody> ().isKinematic = true;
+				item.transform.position = guide.transform.position;
+				item.transform.rotation = guide.transform.rotation;
+				item.transform.parent = tempParent.transform;
+			}
+		}
 	}
 
-	void OnMouseUp(){
-		item.GetComponent<Rigidbody> ().useGravity = true;
-		item.GetComponent<Rigidbody> ().isKinematic = false;
-		item.transform.parent = null;
-		item.transform.position = guide.transform.position;
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("coll");
+			if (Input.GetKeyUp (KeyCode.E)) {
+				Debug.Log ("Key Up");
+				item.GetComponent<Rigidbody> ().useGravity = true;
+				item.GetComponent<Rigidbody> ().isKinematic = false;
+				item.transform.parent = null;
+				item.transform.position = guide.transform.position;
+			}
+		}
 	}
+
+//	void OnTriggerExit(Collider col){
+//		if (col.gameObject.tag == "Player" && Input.GetKeyUp (KeyCode.R)) {
+//			item.GetComponent<Rigidbody> ().useGravity = true;
+//			item.GetComponent<Rigidbody> ().isKinematic = false;
+//			item.transform.parent = null;
+//			item.transform.position = guide.transform.position;
+//		}
+//	}
+//	void OnMouseDown(){
+//		item.GetComponent<Rigidbody> ().useGravity = false;
+//		item.GetComponent<Rigidbody> ().isKinematic = true;
+//		item.transform.position = guide.transform.position;
+//		item.transform.rotation = guide.transform.rotation;
+//		item.transform.parent = tempParent.transform;
+//	}
+//
+//	void OnMouseUp(){
+//		item.GetComponent<Rigidbody> ().useGravity = true;
+//		item.GetComponent<Rigidbody> ().isKinematic = false;
+//		item.transform.parent = null;
+//		item.transform.position = guide.transform.position;
+//	}
 }
