dde4a8031f87ca8fd300cc0a1d7d81e5b29a6f70
scw000000 <scw000000@gmail.com>
2017-12-01 17:30:16 -0500

    Fixed a couple scene errors and improved fire ball

---
+++ Game/Assets/Scripts/ObjectFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollow : MonoBehaviour {
+    public bool _followRotation = false;
+    public bool _followLocation = true;
+    public float _rotationFollowSpeed = 0.3f;
+    public float _locationFollowSpeed = 0.5f;
+    public GameObject _followTarget;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_followTarget == null)
+        {
+            return;
+        }
+        if (_followLocation)
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, _followTarget.transform.position, _locationFollowSpeed);
+        }
+        if (_followRotation)
+        {
+            gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, _followTarget.transform.rotation, _rotationFollowSpeed);
+        }
+	}
+}
