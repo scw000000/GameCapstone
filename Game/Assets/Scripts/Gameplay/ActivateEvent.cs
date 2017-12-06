178d78dd43975263e9d0cb8b555194d44270a289
scw000000 <scw000000@gmail.com>
2017-12-06 17:22:15 -0500

    Fixed scenes
    shadowmap issue in lv1
    sound and moving platform issues in lv3
    sound issue in lv5

---
+++ Game/Assets/Scripts/Gameplay/ActivateEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateEvent : MonoBehaviour {
+    public GameObject[] _gameObjects;
+    public bool[] _setActiveValues;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < _gameObjects.Length; ++i)
+        {
+            _gameObjects[i].SetActive(_setActiveValues[i]);
+        }
+    }
+}
